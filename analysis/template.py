977168d77a54ed6dd5d04969dc2ae66917e09407
Floris Boendermaker <floris.boendermaker@gmail.com>
2022-04-29 15:42:15 +0200

    model fix

--- analysis/template.py
+++ analysis/template.py
@@ -1,4 +1,10 @@
-from ema_workbench import Samplers, perform_experiments, ema_logging, MultiprocessingEvaluator, save_results
+from ema_workbench import (
+    Samplers,
+    perform_experiments,
+    ema_logging,
+    MultiprocessingEvaluator,
+    save_results,
+)
 import pandas as pd
 import numpy as np
 from vadere_ema_formulations import get_vadere_formulation
@@ -10,24 +16,16 @@ from vadere_ema_formulations import get_vadere_formulation
 ema_logging.log_to_stderr(ema_logging.INFO)
 
 # set the right vadere formulations
-model = get_vadere_formulation(
-    id=1,
-    replications=1,
-    model_file='baseCaseData.scenario'
-)
+model = get_vadere_formulation(id=1, replications=1, model_file="baseCaseData.scenario")
 
-if __name__ == '__main__':
-    with MultiprocessingEvaluator(model, n_processes=3) as evaluator:
-        results = evaluator.perform_experiments(
-            scenarios=3, 
-            uncertainty_sampling=Samplers.LHS
-    )
-    
-    # results = perform_experiments(
-    #     model,
-    #     scenarios = 3, 
-    #     uncertainty_sampling=Samplers.LHS
+if __name__ == "__main__":
+    # with MultiprocessingEvaluator(model, n_processes=3) as evaluator:
+    #     results = evaluator.perform_experiments(
+    #         scenarios=3,
+    #         uncertainty_sampling=Samplers.LHS
     # )
+
+    results = perform_experiments(model, scenarios=3, uncertainty_sampling=Samplers.LHS)
 
     # store results
     # save_results(results, '../data/output/EMA/results.tar.gz')
