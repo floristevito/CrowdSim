e3f68560b11635042089985dfd72d1528e242330
Floris Boendermaker <floris.boendermaker@gmail.com>
2022-05-12 16:37:08 +0200

    extra oe

--- analysis/open_exploration.py
+++ analysis/open_exploration.py
@@ -1,8 +1,14 @@
-from ema_workbench import Samplers, perform_experiments, ema_logging, MultiprocessingEvaluator, save_results
-import pandas as pd
 import numpy as np
-from vadere_ema_formulations import get_vadere_formulation
+import pandas as pd
+from ema_workbench import (
+    MultiprocessingEvaluator,
+    Samplers,
+    ema_logging,
+    perform_experiments,
+    save_results,
+)
 
+from vadere_ema_formulations import get_vadere_formulation
 
 """Model run for open exploration"""
 
@@ -11,22 +17,19 @@ ema_logging.log_to_stderr(ema_logging.INFO)
 
 # set the right vadere formulations
 model = get_vadere_formulation(
-    id=1,
-    replications=60,
-    model_file='baseCaseData.scenario'
+    id=1, replications=60, model_file="baseCaseData.scenario"
 )
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with MultiprocessingEvaluator(model, n_processes=20) as evaluator:
         results = evaluator.perform_experiments(
-            scenarios=1000, 
-            uncertainty_sampling=Samplers.LHS
-    )
-    
+            scenarios=2000, uncertainty_sampling=Samplers.LHS
+        )
+
     # results = perform_experiments(
-    #     scenarios = 2, 
+    #     scenarios = 2,
     #     uncertainty_sampling=Samplers.LHS
     # )
 
     # store results
-    save_results(results, '../data/output/EMA/resultsOpenExploration.tar.gz')
+    save_results(results, "../data/output/EMA/resultsOpenExploration2.tar.gz")
