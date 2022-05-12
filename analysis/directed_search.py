07e45ca9053dafa3589fd2bec6110bf88c019410
Floris Boendermaker <floris.boendermaker@gmail.com>
2022-05-12 13:01:18 +0200

    updated directed search

--- analysis/directed_search.py
+++ analysis/directed_search.py
@@ -15,7 +15,9 @@ from vadere_ema_formulations import get_vadere_formulation
 ema_logging.log_to_stderr(ema_logging.INFO)
 
 # set the right vadere formulations
-model = get_vadere_formulation(id=2, replications=1, model_file="baseCaseData.scenario")
+model = get_vadere_formulation(
+    id=2, replications=60, model_file="baseCaseData.scenario"
+)
 
 if __name__ == "__main__":
     # set convergence matrics
@@ -27,10 +29,10 @@ if __name__ == "__main__":
     # search for worst cases(s)
     with MultiprocessingEvaluator(model, n_processes=20) as evaluator:
         results, convergence = evaluator.optimize(
-            nfe=1000,
+            nfe=10000,
             searchover="uncertainties",
             epsilons=[
-                0.25,
+                0.1,
             ]
             * len(model.outcomes),
             convergence=convergence_metrics,
